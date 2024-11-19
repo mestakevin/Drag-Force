7dd6c45c3b9485269fcabfd5872a070f0f158cab
Sara-Talebi <stalebi@g.syr.edu>
2024-11-19 13:50:32 -0500

    Update free_particle.py
    class of particle defined with the vel, pos, and eng

--- free_particle.py
+++ free_particle.py
@@ -1,1 +1,26 @@
 # Particle Class and so on
+class particle:
+    def __init__(self, pos, vel):
+        self.pos = pos
+        self.vel = vel
+        self.mass = 1
+        self.radius = 0
+        self.eng = self.getEng()
+
+    def getEng(self):
+        return 0.5 * self.mass * self.getSpeed() ** 2
+
+    def updEng(self, new_eng):
+        self.eng = new_eng
+
+    def getVel(self):
+        return np.linalg.norm(self.getVel())
+    
+    def updVel(self, new_vel):
+        self.vel = new_vel
+
+    def getPos(self):
+        return self.pos
+
+    def getRadius(self):
+        return self.radius
