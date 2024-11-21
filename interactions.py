69ee3b13c4dbff1d6661a4ea1c89005510c19430
Nicholas Rubayiza M <133821199+Rubayiza1@users.noreply.github.com>
2024-11-20 19:50:59 -0500

    Updates comments in interactions.py

--- interactions.py
+++ interactions.py
@@ -1,11 +1,15 @@
 import numpy as np
 
 class interactions:
-    global M, m
+    global M, m, V
+    
     M = 100  # Mass of sphere
     m = 1  # Mass of each particle
 
-    def collision(velocities):  # velocities should be in [x,y] components
+    def collision(velocities):  # velocities should be in [x,y] components for each particle
+        '''
+        To calculate the momentum exchange for each particle. The sphere is assumed stationary at the instance of collision.
+        '''
         x_velocities = velocities[:,0]  # x_velocities of particles
         y_velocities = velocities[:,1]  # y_velocities of particles
         velocities = []
@@ -17,29 +21,31 @@ class interactions:
             x_v = []
             y_v = []
             
-            # x-direction
-            v1 = x_velocities[i]
-            v2 = v1*(m-M)/(m+M)
-            v3 = v1*(1+(m-M)/(m+M))
+            # in x-direction
+            v1 = x_velocities[i]  # initial x_velocity of particle
+            v2 = v1*(m-M)/(m+M)  # final x_velocity of particle
+            v3 = v1*(1+(m-M)/(m+M))  # final x_velocity of sphere
             x_v.append(v2)
             Mvx.append(v3)
-            
-            # y-direction
-            v1 = y_velocities[i]
-            v2 = v1*(m-M)/(m+M)
-            v3 = v1*(1+(m-M)/(m+M))
+ 
+            # in y-direction
+            v1 = y_velocities[i]  # initial y_velocity of particle
+            v2 = v1*(m-M)/(m+M)  # final y_velocity of particle
+            v3 = v1*(1+(m-M)/(m+M))  # final y_velocity of sphere
             y_v.append(v2)
             Mvy.append(v3)
             
             velocities.append([x_v,y_v])
             i += 1
-            
+
+        
         Vx = np.sum(Mvx)
         Vy = np.sum(Mvy)
         V = [Vx,Vy]
+        
         return velocities, V
 
-    def drag(Vi,dt):
+    def drag(Vi,dt):  # Vi is the initial 2D velocity vector of the sphere, dt is the time step (s).
         '''
         To calculate the force introduced by interactions.
         '''
