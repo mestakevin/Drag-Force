1ae6b3f5693f1ac9fa36a566304bb88c72924619
Jazzman2100 <jasminewatt8@gmail.com>
2024-12-04 23:07:26 -0500

    Update Velocity of the heavy particle v.s. Drag force constant.py

--- Velocity of the heavy particle v.s. Drag force constant.py
+++ Velocity of the heavy particle v.s. Drag force constant.py
@@ -104,7 +104,7 @@ def simulate_drag_force_constant_with_density():
     # Plot the velocity vs drag force constant with air density and temperature
     plt.figure(figsize=(10, 6))
     scatter = plt.scatter(velocities_sorted, drag_force_coefficient_sorted, c=air_density_sorted, cmap='viridis', marker='o', alpha=0.7)
-    plt.colorbar(scatter, label='Temperature coefficient (K)')
+    plt.colorbar(scatter, label='Temperature coefficient (unitless)')
     plt.plot(velocities_sorted, drag_force_coefficient_sorted, color = 'white', alpha = 0.00001)
     plt.xlabel('Velocity (m/s)')
     plt.ylabel('Drag Force Constant (N·s/m²)')
